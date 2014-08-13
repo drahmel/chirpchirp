9a2873e816c51830e64006bcd8074cc0d0ed1fa0
Dan Rahmel <drahmel7@gmail.com>
2014-08-13 11:06:15 -0700

    DJR:Added html and scandir for profile images

--- php/genprofiles.php
+++ php/genprofiles.php
@@ -1,10 +1,24 @@
 <?php
 
 define('ROOT', '/Users/Dan.Rahmel/Sites/chirpchirp/');
+define('NL', "\n");
 require_once(ROOT . "vendor/facedetector/FaceDetector.php");
+$srcPath = ROOT . "etc/INTLFACE/";
+$destPath = ROOT . 'scratch/';
 
 $face_detect = new FaceDetector(ROOT . "vendor/facedetector/detection.dat");
-$face_detect->faceDetect('sample-image1.jpg');
-$face_detect->toJpeg('face1.jpg');
+
+$files = scandir($srcPath);
+foreach($files as $key => $curFile) {
+	$parts = pathinfo($curFile);
+	$ext = !empty($parts['extension']) ?	strtolower($parts['extension']	:	'';
+	print_r($parts);
+	if($key > 10) {
+		break;
+	}
+	echo $curFile . NL;
+}
+//$face_detect->faceDetect('sample-image1.jpg');
+//$face_detect->toJpeg('face1.jpg');
 
 ?>
