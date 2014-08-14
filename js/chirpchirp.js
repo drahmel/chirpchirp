6cefafd5a43016147911b48aff9df8f974d42648
Dan Rahmel <drahmel7@gmail.com>
2014-08-13 21:41:07 -0700

    DJR:Added 3D placeholders

--- js/chirpchirp.js
+++ js/chirpchirp.js
@@ -283,37 +283,71 @@ function displayChatMessage(name, text) {
 
 	setInterval(idleBoids, 1000);
 //});
+	var layers = [],
+		objects = [],
 
-/*
-    Defining our variables
-    world and viewport are DOM elements,
-    worldXAngle and worldYAngle are floats that hold the world rotations,
-    d is an int that defines the distance of the world from the camera
-*/
-var world = document.getElementById( 'world' ),
-    viewport = document.getElementById( 'viewport' ),
-    worldXAngle = 0,
-    worldYAngle = 0,
-    d = 0;
+		world = document.getElementById( 'world' ),
+		viewport = document.getElementById( 'viewport' ),
 
-/*
-    Event listener to transform mouse position into angles
-    from -180 to 180 degress, both vertically and horizontally
-*/
-window.addEventListener( 'mousemove', function( e ) {
-    worldYAngle = -( .5 - ( e.clientX / window.innerWidth ) ) * 180;
-    worldXAngle = ( .5 - ( e.clientY / window.innerHeight ) ) * 180;
-    updateView();
-} );
+		d = 0,
+		p = 400,
+		worldXAngle = 0,
+		worldYAngle = 0;
 
-/*
-    Changes the transform property of world to be
-    translated in the Z axis by d pixels,
-    rotated in the X axis by worldXAngle degrees and
-    rotated in the Y axis by worldYAngle degrees.
-*/
-function updateView() {
-    world.style.transform = 'translateZ( ' + d + 'px ) \
-        rotateX( ' + worldXAngle + 'deg) \
-        rotateY( ' + worldYAngle + 'deg)';
-}
+	viewport.style.webkitPerspective = p;
+	viewport.style.MozPerspective = p;
+	viewport.style.oPerspective = p;
+
+	generate();
+
+	function createCloud() {
+
+		var div = document.createElement( 'div'  );
+		div.className = 'cloudBase';
+		var x = 256 - ( Math.random() * 512 );
+		var y = 256 - ( Math.random() * 512 );
+		var z = 256 - ( Math.random() * 512 );
+		var t = 'translateX( ' + x + 'px ) translateY( ' + y + 'px ) translateZ( ' + z + 'px )';
+		div.style.webkitTransform = t;
+		div.style.MozTransform = t;
+		div.style.oTransform = t;
+		world.appendChild( div );
+
+		return div;
+	}
+
+	window.addEventListener( 'mousewheel', onContainerMouseWheel );
+	window.addEventListener( 'DOMMouseScroll', onContainerMouseWheel );
+
+	window.addEventListener( 'mousemove', function( e ) {
+		worldYAngle = -( .5 - ( e.clientX / window.innerWidth ) ) * 180;
+		worldXAngle = ( .5 - ( e.clientY / window.innerHeight ) ) * 180;
+		updateView();
+	} );
+
+	function generate() {
+		objects = [];
+		if ( world.hasChildNodes() ) {
+			while ( world.childNodes.length >= 1 ) {
+				world.removeChild( world.firstChild );
+			}
+		}
+		for( var j = 0; j < 5; j++ ) {
+			objects.push( createCloud() );
+		}
+	}
+
+	function updateView() {
+		var t = 'translateZ( ' + d + 'px ) rotateX( ' + worldXAngle + 'deg) rotateY( ' + worldYAngle + 'deg)';
+		world.style.webkitTransform = t;
+		world.style.MozTransform = t;
+		world.style.oTransform = t;
+	}
+
+	function onContainerMouseWheel( event ) {
+
+		event = event ? event : window.event;
+		d = d - ( event.detail ? event.detail * -5 : event.wheelDelta / 8 );
+		updateView();
+
+	}
