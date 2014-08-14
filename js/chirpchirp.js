eee8437c4a3078b8d6e0cf613cbb415a09a6de40
Dan Rahmel <drahmel7@gmail.com>
2014-08-13 21:37:01 -0700

    DJR:Added CSS transform

--- js/chirpchirp.js
+++ js/chirpchirp.js
@@ -217,14 +217,19 @@ function idleBoids() {
 	document.bee.update();
 }
 var boidieNum = 0;
+var deadBoidies = 0;
 
 creaturesRef.on('child_added', function(snapshot) {
 	var imageStr;
 	var boid = snapshot.val();
 	var age = getLinuxTime() - boid.deathAge;
 	if(age > 0) {
+		if(deadBoidies > 20) {
+			return;
+		}
 		imageStr = '<img src="images/songbird.png" class="dead" style="width:128px;" />';
 		var boidieSize = 128;
+		deadBoidies++;
 		//return;
 	} else {
 		var totalAge = boid.deathAge-boid.birthTime;
@@ -279,3 +284,36 @@ function displayChatMessage(name, text) {
 	setInterval(idleBoids, 1000);
 //});
 
+/*
+    Defining our variables
+    world and viewport are DOM elements,
+    worldXAngle and worldYAngle are floats that hold the world rotations,
+    d is an int that defines the distance of the world from the camera
+*/
+var world = document.getElementById( 'world' ),
+    viewport = document.getElementById( 'viewport' ),
+    worldXAngle = 0,
+    worldYAngle = 0,
+    d = 0;
+
+/*
+    Event listener to transform mouse position into angles
+    from -180 to 180 degress, both vertically and horizontally
+*/
+window.addEventListener( 'mousemove', function( e ) {
+    worldYAngle = -( .5 - ( e.clientX / window.innerWidth ) ) * 180;
+    worldXAngle = ( .5 - ( e.clientY / window.innerHeight ) ) * 180;
+    updateView();
+} );
+
+/*
+    Changes the transform property of world to be
+    translated in the Z axis by d pixels,
+    rotated in the X axis by worldXAngle degrees and
+    rotated in the Y axis by worldYAngle degrees.
+*/
+function updateView() {
+    world.style.transform = 'translateZ( ' + d + 'px ) \
+        rotateX( ' + worldXAngle + 'deg) \
+        rotateY( ' + worldYAngle + 'deg)';
+}
