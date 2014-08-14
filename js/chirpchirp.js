db66e2b607f69232109ed1ac310bf5d524da91c7
Dan Rahmel <drahmel7@gmail.com>
2014-08-13 22:26:05 -0700

    DJR:Added clouds

--- js/chirpchirp.js
+++ js/chirpchirp.js
@@ -283,6 +283,33 @@ function displayChatMessage(name, text) {
 
 	setInterval(idleBoids, 1000);
 //});
+
+
+	(function() {
+		var lastTime = 0;
+		var vendors = ['ms', 'moz', 'webkit', 'o'];
+		for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+			window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+			window.cancelRequestAnimationFrame = window[vendors[x]+
+			  'CancelRequestAnimationFrame'];
+		}
+
+		if (!window.requestAnimationFrame)
+			window.requestAnimationFrame = function(callback, element) {
+				var currTime = new Date().getTime();
+				var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+				var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+				  timeToCall);
+				lastTime = currTime + timeToCall;
+				return id;
+			};
+
+		if (!window.cancelAnimationFrame)
+			window.cancelAnimationFrame = function(id) {
+				clearTimeout(id);
+			};
+	}())
+
 	var layers = [],
 		objects = [],
 
@@ -314,7 +341,14 @@ function displayChatMessage(name, text) {
 		world.appendChild( div );
 
 		for( var j = 0; j < 5 + Math.round( Math.random() * 10 ); j++ ) {
-			var cloud = document.createElement( 'div' );
+			var cloud = document.createElement( 'img' );
+			cloud.style.opacity = 0;
+			var r = Math.random();
+			var src = 'cloud.png';
+			( function( img ) { img.addEventListener( 'load', function() {
+				img.style.opacity = .8;
+			} ) } )( cloud );
+			cloud.setAttribute( 'src', src );
 			cloud.className = 'cloudLayer';
 
 			var x = 256 - ( Math.random() * 512 );
@@ -328,7 +362,8 @@ function displayChatMessage(name, text) {
 				y: y,
 				z: z,
 				a: a,
-				s: s
+				s: s,
+				speed: .1 * Math.random()
 			};
 			var t = 'translateX( ' + x + 'px ) translateY( ' + y + 'px ) translateZ( ' + z + 'px ) rotateZ( ' + a + 'deg ) scale( ' + s + ' )';
 			cloud.style.webkitTransform = t;
@@ -341,7 +376,6 @@ function displayChatMessage(name, text) {
 
 		return div;
 	}
-
 
 	window.addEventListener( 'mousewheel', onContainerMouseWheel );
 	window.addEventListener( 'DOMMouseScroll', onContainerMouseWheel );
@@ -370,7 +404,7 @@ function displayChatMessage(name, text) {
 		world.style.MozTransform = t;
 		world.style.oTransform = t;
 	}
-
+/*
 	function onContainerMouseWheel( event ) {
 
 		event = event ? event : window.event;
@@ -378,3 +412,21 @@ function displayChatMessage(name, text) {
 		updateView();
 
 	}
+*/
+	function update (){
+
+		for( var j = 0; j < layers.length; j++ ) {
+			var layer = layers[ j ];
+			layer.data.a += layer.data.speed;
+			var t = 'translateX( ' + layer.data.x + 'px ) translateY( ' + layer.data.y + 'px ) translateZ( ' + layer.data.z + 'px ) rotateY( ' + ( - worldYAngle ) + 'deg ) rotateX( ' + ( - worldXAngle ) + 'deg ) rotateZ( ' + layer.data.a + 'deg ) scale( ' + layer.data.s + ')';
+			layer.style.webkitTransform = t;
+			layer.style.MozTransform = t;
+			layer.style.oTransform = t;
+		}
+
+		requestAnimationFrame( update );
+
+	}
+
+	//update();
+
