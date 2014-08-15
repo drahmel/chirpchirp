9f18644163211e7d795493db5d06c9364b7927bc
Dan Rahmel <drahmel7@gmail.com>
2014-08-15 09:31:02 -0700

    DJR:Added random tweet accounts

--- js/twitterTest.js
+++ js/twitterTest.js
@@ -1,11 +1,19 @@
 var twitterAPI = require('./tweet.js');
-var tweetr = new twitterAPI('MichaelDHyson');
+
+var boidiesAccounts = ['MichaelDHyson', 'lynnmbessinger', 'halmrippetoe', 'jilliancmarotz'];
+var num = Math.floor( Math.random() *  boidiesAccounts.length);
+
+var tweetr = new twitterAPI(boidiesAccounts[num]);
 var utilsAPI = require('./utils.js');
 var utils = new utilsAPI();
 
 
 //doSearch("fitness since:2014-08-08");
-tweetr.doSearch("fitness", function(result) {
+var term = "fitness";
+var term = "health fitness";
+var term = "gym";
+
+tweetr.doSearch(term, function(result) {
 	tweetr.retweetPopular(result);
 });
 //doPost("The scale doesn't lie.");
